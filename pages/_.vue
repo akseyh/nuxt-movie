7b9fd173d892bf3c3097400c5d81b40892e8bab8
Şems Yılmaz <yilmazsems@gmail.com>
2020-12-13 01:27:29 +0300

    404 page added

---
+++ pages/_.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-screen text-5xl">
+        <b>404 - Page Not Found</b>
+        <button @click="$router.push({name: 'index'})">Back To Home</button>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'full_page'
+}
+</script>
