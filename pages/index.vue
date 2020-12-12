5bdfc638bec52a51df44f35c1430044321d38c78
Şems Yılmaz <yilmazsems@gmail.com>
2020-12-12 23:14:55 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center">
+    <b class="title">NUXT MOVIE</b>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.title {
+  font-size: 50px;
+}
+</style>
