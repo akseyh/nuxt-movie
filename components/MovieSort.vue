412cc3b312f751413ed8ee7dea823292b27be730
Şems Yılmaz <yilmazsems@gmail.com>
2020-12-13 19:38:22 +0300

    search button added

--- components/MovieSort.vue
+++ components/MovieSort.vue
@@ -11,6 +11,10 @@
         <FilterPanelCard title="Where To Watch" style="margin-top: 12px;">
 
         </FilterPanelCard>
+        
+        <div class="search_field">
+            <p>Search</p>
+        </div>
     </div>
 </template>
 
@@ -45,4 +49,21 @@ export default {
         font-size: 1em
         font-weight: 300
         margin-bottom: 10px
+    .search_field
+        width: 100%
+        background-color: rgba(228,228,228, 0.7)
+        backdrop-filter: blur(10px)
+        cursor: default
+        margin-top: 20px
+        display: flex
+        justify-content: center
+        align-items: center
+        align-content: center
+        border-radius: 20px
+        height: 44px
+        p
+            font-weight: 600
+            color: rgba(0,0,0,0.5)
+
+        
 </style>
